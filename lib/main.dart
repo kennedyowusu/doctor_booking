a1284b534cbc2c1898774d8ae7674d3af7967794
kennedyowusu <kennediowusu@gmail.com>
2023-03-02 05:23:50 +0000

    set up the project

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:doctor_booking/utils/config.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Doctor Appointment Booking',
+      theme: ThemeData(
+        //pre-define input decoration
+        inputDecorationTheme: const InputDecorationTheme(
+          focusColor: Config.primaryColor,
+          border: Config.outlinedBorder,
+          focusedBorder: Config.focusBorder,
+          errorBorder: Config.errorBorder,
+          enabledBorder: Config.outlinedBorder,
+          floatingLabelStyle: TextStyle(color: Config.primaryColor),
+          prefixIconColor: Colors.black38,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Config.primaryColor,
+          selectedItemColor: Colors.white,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          unselectedItemColor: Colors.grey.shade700,
+          elevation: 10,
+          type: BottomNavigationBarType.fixed,
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+        // '/': (context) => const AuthPage(),
+        // 'main': (context) => const MainLayout(),
+        // 'booking_page': (context) => BookingPage(),
+        // 'success_booking': (context) => const AppointmentBooked(),
+      },
+    );
+  }
+}
